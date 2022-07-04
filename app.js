854984225da6f37122cfad91dbb63d1b0a44742f
DELL <siralex2411@gmail.com>
2022-07-04 11:30:12 +0700

    upload source code

---
+++ app.js
@@ -0,0 +1,29 @@
+// menu mobile & scroll-top
+ 
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+
+  if(window.scrollY > 60){
+    document.querySelector('#scroll-top').classList.add('active');
+  }else{
+    document.querySelector('#scroll-top').classList.remove('active');
+  }
+
+  if(window.scrollY > 60){
+    document.querySelector('.header').classList.add('fixed');
+  }else{
+    document.querySelector('.header').classList.remove('fixed');
+  }
+};
+
+
+
